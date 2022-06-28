465dd4a22d119216ff29be14225477f4937f9810
Graham <thegrahamprice@icloud.com>
2022-06-28 09:11:59 -0600

    commit tree 79e6fb5120b59924cac5413782211abdf002ec98

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+function distanceFromHQInBlocks(blockNumber) {
+    if (blockNumber > 42) {
+        return blockNumber - 42;
+    } else {
+        return 42 - blockNumber;
+    }
+}
+    function distanceFromHQInFeet (blockNumber) {
+        return distanceFromHQInBlocks(blockNumber) * 264;
+        }        
+    
+    
+   function distanceTravelledInFeet (start, destination) {
+    if (start < destination) {
+        return (destination -start) * 264;    
+    } else {
+        return (start-destination);
+    }
+   }
